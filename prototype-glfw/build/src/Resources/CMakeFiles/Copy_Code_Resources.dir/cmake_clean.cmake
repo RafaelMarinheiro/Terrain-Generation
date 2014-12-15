7ab1b6367bccd4001920a33307bf82cb114b1c48
Rafael Marinheiro <rafael.fariasmarinheiro@gmail.com>
2014-12-14 20:03:38 -0500

    REMOVE THE BUILD

--- prototype-glfw/build/src/Resources/CMakeFiles/Copy_Code_Resources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Copy_Code_Resources"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/Copy_Code_Resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
